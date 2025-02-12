3f47ae3bfaf2625731835ac3f0bf3f73138e147a
mcartage32 <mcartage33@gmail.com>
2025-02-11 23:33:26 -0500

    Se configura jest para generar reporte de las pruebas y su cobertura

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true, // Para incluir la cobertura de pruebas
   coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'lcov', 'text', 'html'], // Formatos de reporte
+  reporters: [
+    'default',
+    ['jest-junit', { outputDirectory: 'docs', outputName: 'test-results.xml' }]
+  ],
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
   },
